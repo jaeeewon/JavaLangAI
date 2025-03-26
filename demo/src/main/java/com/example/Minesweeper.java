120977aa1d6a13b8f84b9f0453dd2ff8e6d3c68a
PleizzChoi <hello.pleizz@gmail.com>
2025-03-26 21:52:25 +0900

    implemented assignment01: Minesweeper.java

--- demo/src/main/java/com/example/Minesweeper.java
+++ demo/src/main/java/com/example/Minesweeper.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,6 +12,11 @@ public class Minesweeper {
     private final int[][] mineBoard; // Underlying board: -1 for mine, otherwise number of adjacent mines
     private final boolean[][] revealed;
     private final boolean[][] flagged;
+    private static final int[][] NEARBY = {
+            { -1, -1 }, { -1, 0 }, { -1, 1 },
+            { 0, -1 }, { 0, 1 },
+            { 1, -1 }, { 1, 0 }, { 1, 1 }
+    };
 
     public Minesweeper(int size, int totalMines) {
         this.rows = size;
@@ -22,7 +28,23 @@ public class Minesweeper {
         flagged = new boolean[size][size];
         initializeBoard();
         placeMines();
-        // calculateNumbers();
+        calculateNumbers();
+    }
+
+    private void calculateNumbers() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mineBoard[i][j] == -1)
+                    continue;
+
+                for (int[] dir : NEARBY) {
+                    int ni = i + dir[0], nj = j + dir[1];
+                    if (ni >= 0 && nj >= 0 && ni < rows && nj < cols && mineBoard[ni][nj] == -1) {
+                        mineBoard[i][j]++;
+                    }
+                }
+            }
+        }
     }
 
     // Randomly place mines on the board.
@@ -57,19 +79,49 @@ public class Minesweeper {
     // Display the current state of the visible board.
     private void printBoard() {
         // Print column indices.
-        System.out.print("   ");
+        System.out.print("    ");
         for (int j = 0; j < cols; j++) {
             System.out.print(j + " ");
         }
         System.out.println();
-        System.out.println("  -----------------");
+        System.out.println("   -------------------");
+        int revealedCount = 0;
         for (int i = 0; i < rows; i++) {
             System.out.print(i + " | ");
             for (int j = 0; j < cols; j++) {
                 System.out.print(board[i][j] + " ");
+                revealedCount += (board[i][j] >= '0') ? 1 : 0;
             }
-            System.out.println();
+            System.out.println(Arrays.toString(mineBoard[i]));
         }
+        System.out.printf("%d tiles left to win the game!\n", this.rows * this.cols - revealedCount - this.totalMines);
+    }
+
+    private void openNearby(int i, int j) {
+        // 재귀적으로 오픈
+        if (i < 0 || j < 0 || i >= rows || j >= cols || mineBoard[i][j] == -1 || board[i][j] != '.')
+            return;
+
+        board[i][j] = (char) (mineBoard[i][j] + '0');
+        revealed[i][j] = true;
+
+        if (mineBoard[i][j] == 0) { // 지뢰 근처 아닐 때만
+            for (int[] dir : NEARBY) {
+                openNearby(i + dir[0], j + dir[1]);
+            }
+        }
+    }
+
+    private boolean isGameEnd() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mineBoard[i][j] != -1 && !revealed[i][j]) {
+                    // System.out.printf("[%d][%d] is not revealed though it is 지뢰", i, j);
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // Main game loop.
@@ -84,6 +136,30 @@ public class Minesweeper {
             int r = scanner.nextInt();
             int c = scanner.nextInt();
 
+            if (r < this.rows && c < this.cols) {
+                if (command.equals("r")) {
+                    if (mineBoard[r][c] == -1) {
+                        gameRunning = false;
+                        System.out.println("YOU LOSE!");
+                        continue;
+                    } else {
+                        openNearby(r, c);
+                        if (isGameEnd()) {
+                            System.out.printf("YOU WIN!");
+                            break;
+                        }
+                    }
+                } else if (command.equals("f")) {
+                    if (revealed[r][c]) {
+                        System.out.println("THIS POINT IS REVEALED!");
+                        continue;
+                    } else {
+                        flagged[r][c] = true;
+                        board[r][c] = 'F';
+                    }
+                }
+            }
+
             clearScreen();
         }
         scanner.close();
@@ -95,6 +171,8 @@ public class Minesweeper {
         int fieldSize = scanner.nextInt();
         System.out.print("The number of mines: ");
         int numMines = scanner.nextInt();
+        // int fieldSize = 9; // hard-coded
+        // int numMines = 10; // hard-coded
         // Initialize a 9x9 board with 10 mines.
         Minesweeper game = new Minesweeper(fieldSize, numMines);
         game.play();
